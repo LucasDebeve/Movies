764cc4da4d1b5fff0851d3b8b0047a7887b885a2
debe0033 <lucas.debeve@etudiant.univ-reims.fr>
2023-06-13 16:28:30 +0200

    Ajout de la classe GanreMovie

---
+++ src/Entity/GenreMovie.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Entity;
+
+class GenreMovie
+{
+    private int $movieId;
+    private int $genreId;
+
+    /** Accesseur du movieId
+     * @return int
+     */
+    public function getMovieId(): int
+    {
+        return $this->movieId;
+    }
+
+    /** Modifier le movieId
+     * @param int $movieId
+     * @return GenreMovie
+     */
+    public function setMovieId(int $movieId): GenreMovie
+    {
+        $this->movieId = $movieId;
+        return $this;
+    }
+
+    /** Accesseur du genreId
+     * @return int
+     */
+    public function getGenreId(): int
+    {
+        return $this->genreId;
+    }
+
+    /** Modifier le genreId
+     * @param int $genreId
+     * @return GenreMovie
+     */
+    public function setGenreId(int $genreId): GenreMovie
+    {
+        $this->genreId = $genreId;
+        return $this;
+    }
+}
