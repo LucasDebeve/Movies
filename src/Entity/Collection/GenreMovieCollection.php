06f51c1c73dc818c055ce7c6332b46f3d456334b
debe0033 <lucas.debeve@etudiant.univ-reims.fr>
2023-06-13 17:04:46 +0200

    Ajout de la classe GenreMovieCollection

---
+++ src/Entity/Collection/GenreMovieCollection.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use Entity\GenreMovie;
+
+class GenreMovieCollection
+{
+    public static function findByMovieId(int $movieId) : array
+    {
+        $stmt = MyPdo::getInstance()->prepare(<<<SQL
+SELECT * FROM movie_genre WHERE movieid = :movieId
+SQL);
+        $stmt->execute([":movieId" => $movieId]);
+        return $stmt->fetchAll(MyPdo::FETCH_CLASS, GenreMovie::class);
+    }
+
+    public static function findByGenreId(int $genreId) : array
+    {
+        $stmt = MyPdo::getInstance()->prepare(<<<SQL
+SELECT * FROM movie_genre WHERE genreid = :genreId
+SQL);
+        $stmt->execute([":genreId" => $genreId]);
+        return $stmt->fetchAll(MyPdo::FETCH_CLASS, GenreMovie::class);
+    }
+
+}
